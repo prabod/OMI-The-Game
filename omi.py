85c078c0c3655bff648cc7259d1c73c248d1105c
prabod manuranga rathnayaka <praboderathnayaka@gmail.com>
2014-04-05 12:14:11 +0530

    Create omi.py

---
+++ omi.py
@@ -0,0 +1,107 @@
+"""
+#***********************************************************************#
+# Copyright (C) 2014 Prabod Rathnayaka									#
+# This file is part of OMI-The Game.									#
+#																		#
+# OMI-The Game is free software: you can redistribute it and/or modify  #
+# it under the terms of the GNU General Public License as published by  #
+# the Free Software Foundation, either version 3 of the License, or     #
+# any later version.													#
+#																		#
+# OMI-The Game is distributed in the hope that it will be useful,		#
+# but WITHOUT ANY WARRANTY; without even the implied warranty of		#
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the			#
+# GNU General Public License for more details.							#
+#																		#
+# You should have received a copy of the GNU General Public License		#
+#***********************************************************************#
+
+"""
+
+
+#Imports
+import math
+import random
+
+from Tkinter import *
+from Canvas import Rectangle, CanvasText, Group, Window
+
+#########################################################################
+class Cards:
+	"""
+
+	Class that used to Shuffle cards,Divide cards among four players,and 
+	card Drawing instuctions to cpu players.
+
+	"""
+	def __init__(self):
+		self.CardDeck = range(0,32) #Saves the card deck into a list
+		self.Dummy=[0,0,0,0] # dummy for choose fovourite
+		self.temp = [] # Temp for check 
+		self.difsuit=True
+		
+	def DivideRound1(self,player1,player2,player3,player4):
+	
+		""" Shuffling until gets 2 or more from same suit """ 
+		while self.difsuit:
+				random.shuffle(self.CardDeck) #shuffle the cards
+				for i in CardDeck[0:4]:  
+					if i//8==0:
+						self.temp.append(0) 
+				
+					if i//8==1:
+						self.temp.append(1)
+				
+					if i//8==2:
+						self.temp.append(2)
+				
+					if i//8==3:
+						self.temp.append(3)
+						
+				if temp.count(0) == temp.count(1) == temp.count(2) == temp.count(3):
+					random.shuffle(self.CardDeck) #shuffle the cards
+				else:
+					self.difsuit=False
+					
+		for i in range(4):
+			player1.append(self.CardDeck[i])  #Four Cards to Player1
+		
+		for i in range(4,8):
+			player2.append(self.CardDeck[i])  #Four Cards to Player2
+		
+		for i in range(8,12):
+			player3.append(self.CardDeck[i])  #Four Cards to Player3
+		
+		for i in range(12,16):
+			player4.append(self.CardDeck[i])  #Four Cards to Player4
+	
+	def DivideRound2(self,player1,player2,player3,player4):		
+		for i in range(16,20):
+			player1.append(self.CardDeck[i])  #Four Cards to Player1
+		
+		for i in range(20,24):
+			player2.append(self.CardDeck[i])  #Four Cards to Player2
+		
+		for i in range(24,28):
+			player3.append(self.CardDeck[i])  #Four Cards to Player3
+		
+		for i in range(28,32):
+			player4.append(self.CardDeck[i])  #Four Cards to Player4	
+				
+	def Favourite(self,playerX): #Choosing Favourite
+		for i in range(4):
+			if playerX[i]//8==0:
+				self.Dummy[0]+=playerX[i]%8 
+			
+			if playerX[i]//8==1:
+				self.Dummy[1]+=playerX[i]%8
+				
+			if playerX[i]//8==2:
+				self.Dummy[2]+=playerX[i]%8
+				
+			if playerX[i]//8==3:
+				self.Dummy[3]+=playerX[i]%8
+			
+		return self.Dummy.index(max(self.Dummy))
+			
+##########################################################################
