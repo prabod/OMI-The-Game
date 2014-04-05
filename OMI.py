54c291097ae049683281527f96a4c7d553ea161f
Prabod Rathnayaka <praboderathnayaka@gmail.com>
2014-04-05 14:33:57 +0530

    Created the Algo for Fav prompting and stuff

--- OMI.py
+++ OMI.py
@@ -106,3 +106,75 @@ class Cards:
 			
 ##########################################################################
 
+class RotatePlayer:
+	def __init__(self,South,East,North,West,max,choosefav,Fav,master):
+		self.Player1 = [] 
+		self.Player2 = [] 
+		self.Player3 = [] 
+		self.Player4 = [] 
+	
+	def DivideCards(self,self.Player1,self.Player2,self.Player3,self.Player4): #Divide cards to players
+		Card=Cards() #call Cards class
+		Card.DivideRound1(self.Player1,self.Player2,self.Player3,self.Player4)
+		if South == choosefav: #Prompting for Fav from the user
+			self.w=popupFav(master)
+        	self.master.wait_window(self.w.top)
+        	Fav=self.w.value
+		else: #Choosing Fav by CPU Plyers
+			Fav=card.Favourite(choosefav)
+			Card.DivideRound2(self.Player1,self.Player2,self.Player3,self.Player4)
+			
+	def Chooseplayer(self,South,East,North,West):
+		if max == South:
+			South = self.Player1
+			East = self.Player2
+			North = self.Player3
+			West = self.Player4
+		elif max == East:
+			East = self.Player1
+			North = self.Player2
+			West = self.Player3
+			South = self.Player4
+		elif max == North:
+			North = self.Player1
+			West = self.Player2
+			South = self.Player3
+			East = self.Player4
+		else:
+			West = self.Player1
+			South = self.Player2
+			East = self.Player3
+			North = self.Player4
+			
+#######################################################################################			
+class popupFav(object): #popup window for Fav prompt for user
+    def __init__(self,master):
+        top=self.top=Toplevel(master)
+        self.l=Label(top,text="තුරුම්පු තෝරන්න")
+        self.l.pack()
+        self.b0=Button(top,image=PhotoImage(file = "image/spade.gif"),command=self.spade)
+        self.b0.pack(side=LEFT)
+        
+        self.b1=Button(top,image=PhotoImage(file = "image/club.gif"),command=self.club)
+        self.b1.pack(side=LEFT)
+        
+        self.b2=Button(top,image=PhotoImage(file = "image/diamond.gif"),command=self.diamond)
+        self.b2.pack(side=LEFT)
+        
+        self.b3=Button(top,image=PhotoImage(file = "image/heart.gif"),command=self.heart)
+        self.b3.pack(side=LEFT)
+        
+    def spade(self):
+        self.value=0
+        self.top.destroy()
+	def club(self):
+        self.value=1
+        self.top.destroy()
+    def diamond(self):
+        self.value=2
+        self.top.destroy()
+    def heart(self):
+        self.value=3
+        self.top.destroy()
+#########################################################################################
+
